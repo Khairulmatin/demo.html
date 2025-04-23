5050c9f9deebccc2d8fcb2ee8534eb4b446ed8ef
Khairulmatin <khairulmatin302@gmail.com>
2025-04-23 15:18:53 +0530

    save to master

--- Demo.java
+++ Demo.java
@@ -1,4 +1,4 @@
-public class Demo {
+public class demo {
     public static void main(String[] args){
         int num1=10;
         int num2=5;
@@ -7,6 +7,9 @@ public class Demo {
 
         int diff= num1 -num2;
         System.out.println("diff:"+diff);
+
+        System.out.println("multiplication"+(num1*num2));
+        System.out.println("div:"+(num1/num2));
 
 
     }
